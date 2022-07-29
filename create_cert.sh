051549daa069ab696a246f689ffa6e5fbcc16a49
Bill Horvath <bhorvath@umich.edu>
2022-07-29 14:57:24 -0400

    Added ability to specify additional host names as DNS subject alternative names on the command line to allow for certs covering more than one host.

--- create_cert.sh
+++ create_cert.sh
@@ -5,7 +5,7 @@
 
 if [[ $# == 0 ]]
 then
-  echo "Usage: sh create_cert.sh <host name>"
+  echo "Usage: sh create_cert.sh <host name> [subjectAlternativeName1 subjectAlternativeName2 ...]"
   exit 0
 fi
 env_error=1
@@ -38,19 +38,40 @@ then
   source "$THIS_DIR/.dev_env"
 fi
 
-if ! [[ -n $TLS_DIR \
+
+TLS_DIR
+COUNTRY
+STATE
+LOCALE
+ORGANIZATION
+ORGANIZATIONAL_UNIT
+CERTS_DIR
+REVOKED_CERTS_DIR
+PRIVATE_KEY_DIR
+OPENSSL_CONF_PATH
+OPENSSL_EXT_PATH
+OPENSSL_PASSIN_PATH
+ROOT_CA_PATH
+INTERMEDIATE_CA_KEY_PATH
+INTERMEDIATE_CA_BUNDLE_PATH
+
+if ! [[ \
+-n $TLS_DIR \
 && -n $COUNTRY \
 && -n $STATE \
 && -n $LOCALE \
 && -n $ORGANIZATION \
 && -n $ORGANIZATIONAL_UNIT \
 && -n $CERTS_DIR \
+&& -n $REVOKED_CERTS_DIR
 && -n $PRIVATE_KEY_DIR \
-&& -n $INTERMEDIATE_CA_KEY_PATH \
-&& -n $INTERMEDIATE_CA_BUNDLE_PATH \
 && -n $OPENSSL_CONF_PATH \
 && -n $OPENSSL_EXT_PATH \
-&& -n $OPENSSL_PASSIN_PATH ]] ;
+&& -n $OPENSSL_PASSIN_PATH \
+&& -n $INTERMEDIATE_CA_KEY_PATH \
+&& -n $INTERMEDIATE_CA_BUNDLE_PATH \
+&& -n $ROOT_CA_PATH \
+]] ;
 then
   echo "All needed environment variables aren't specified:
   TLS_DIR: $TLS_DIR
@@ -60,12 +81,14 @@ then
   ORGANIZATION: $ORGANIZATION
   ORGANIZATIONAL_UNIT: $ORGANIZATIONAL_UNIT
   CERTS_DIR: $CERTS_DIR
+  REVOKED_CERTS_DIR: $REVOKED_CERTS_DIR
   PRIVATE_KEY_DIR: $PRIVATE_KEY_DIR
-  INTERMEDIATE_CA_KEY_PATH: $INTERMEDIATE_CA_KEY_PATH
-  INTERMEDIATE_CA_BUNDLE_PATH: $INTERMEDIATE_CA_BUNDLE_PATH
   OPENSSL_CONF_PATH: $OPENSSL_CONF_PATH
   OPENSSL_EXT_PATH: $OPENSSL_EXT_PATH
-  OPENSSL_PASSIN_PATH: $OPENSSL_PASSIN_PATH"
+  OPENSSL_PASSIN_PATH: $OPENSSL_PASSIN_PATH
+  INTERMEDIATE_CA_KEY_PATH: $INTERMEDIATE_CA_KEY_PATH
+  INTERMEDIATE_CA_BUNDLE_PATH: $INTERMEDIATE_CA_BUNDLE_PATH
+  ROOT_CA_PATH: $ROOT_CA_PATH"
   exit $env_error
 fi
 
@@ -89,8 +112,9 @@ fi
 
 HOST_KEY_PATH="$PRIVATE_KEY_DIR/$HOST_NAME-key.pem"
 HOST_CSR_PATH="$CERTS_DIR/$HOST_NAME.csr"
-HOST_CERT_PATH="$CERTS_DIR/$HOST_NAME.pem"
+HOST_CERT_PATH="$CERTS_DIR/$HOST_NAME.crt"
 HOST_CA_BUNDLE_PATH="$CERTS_DIR/$HOST_NAME-ca-bundle.pem"
+TEMP_EXT_PATH="$THIS_DIR/temp_server_ext.cnf"
 
 remove_path () {
   if [[ -a "$1" ]]
@@ -114,7 +138,7 @@ fi
 if [[ $overwrite == "true" || ! -a "$HOST_CERT_PATH" ]]
 then
   echo "Creating new host certificate for $HOST_NAME..."
-  if [[ -a "$HOST_KEY_PATH" ]]
+  if [[ -a "$HOST_KEY_PATH" && $overwrite == "true" ]]
   then
     echo "Revoking existing $HOST_NAME certificate..."
     if openssl ca \
@@ -150,29 +174,48 @@ then
 
   echo "Creating certificate signing request..."
   CERT_SUBJ="/C=$COUNTRY/ST=$STATE/L=$LOCALE/O=$ORGANIZATION/OU=$ORGANIZATIONAL_UNIT/CN=$HOST_NAME"
+  if [[ $# -gt 1 ]] ;
+  then
+    sans="\n"
+    i=0
+    argv[-1]=()
+    while [[ -n ${argv[-1]} ]]
+    do {
+      sans+="DNS.$i = ${argv[-1]}\n"
+      argv[-1]=()
+      i=$((i + 1))
+    }
+    done
+    cp "$OPENSSL_EXT_PATH" "$TEMP_EXT_PATH"
+    echo "$sans"
+    echo "$sans" >> "$TEMP_EXT_PATH"
+    unset OPENSSL_EXT_PATH && OPENSSL_EXT_PATH="$TEMP_EXT_PATH"
+  fi
+
   if ! openssl req \
-  -new \
-  -key "$HOST_KEY_PATH" \
-  -out "$HOST_CSR_PATH" \
-  -subj "$CERT_SUBJ" \
-  -passin "file:$OPENSSL_PASSIN_PATH" ;
+    -new \
+    -key "$HOST_KEY_PATH" \
+    -out "$HOST_CSR_PATH" \
+    -subj "$CERT_SUBJ" \
+    -passin "file:$OPENSSL_PASSIN_PATH" ;
   then
     echo "Error creating certificate signing request!"
     if [[ -a "$HOST_CSR_PATH" ]]
     then
-      remove_path "$HOST_CSR_PATH"
+      remove_path "$HOST_CSR_PATH"d
     fi
     exit $cert_error
   fi
 
   if ! openssl ca \
   -batch \
+  -notext \
   -cert "$INTERMEDIATE_CA_BUNDLE_PATH" \
   -config "$OPENSSL_CONF_PATH" \
   -extfile "$OPENSSL_EXT_PATH" \
   -in "$HOST_CSR_PATH" \
   -out "$HOST_CERT_PATH" \
-  -days 365 \
+  -days 3650 \
   -passin "file:$OPENSSL_PASSIN_PATH" \
   -keyfile "$INTERMEDIATE_CA_KEY_PATH" ;
   then
@@ -184,6 +227,10 @@ then
   then
     echo "Error converting server certificate to PEM!"
     exit $cert_error
+  fi
+  if [[ -a "$TEMP_EXT_PATH" ]]
+  then
+    rm "$TEMP_EXT_PATH"
   fi
   else
     echo "Using existing certificate..."
@@ -207,13 +254,14 @@ echo "Removing podman secrets..."
 source "$THIS_DIR/remove_podman_secret.sh"
 remove_secret "${HOST_NAME}_cert_key" $podman_error
 remove_secret "${HOST_NAME}_cert_pub" $podman_error
-remove_secret "${HOST_NAME}_ca_bundle_pub" $podman_error
+remove_secret "${HOST_NAME}_cert_bundle_pub" $podman_error
 
 echo "Creating podman secrets..."
 source "$THIS_DIR/create_podman_secret.sh"
 create_secret "${HOST_NAME}_cert_key" "$HOST_KEY_PATH" $podman_error
 create_secret "${HOST_NAME}_cert_pub" "$HOST_CERT_PATH" $podman_error
-create_secret "${HOST_NAME}_ca_bundle_pub" "$HOST_CA_BUNDLE_PATH" $podman_error
+create_secret "${HOST_NAME}_cert_bundle_pub" "$HOST_CA_BUNDLE_PATH" $podman_error
+
 echo "Created podman secrets: ${HOST_NAME}_cert_key, ${HOST_NAME}_cert_pub, ${HOST_NAME}_ca_bundle_pub"
 
 export HOST_KEY_PATH
